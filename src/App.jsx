435d5f68135f9ff910811b1dc875169fa3b35b48
OG0808 <ogarns@proton.me>
2023-12-11 04:33:23 -0500

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import Nabvar from "./components/Nabvar/Nabvar";
 import Home from "./pages/Home";
 import CardCountry from "./pages/CardCountry";
+import useCountryStore from "./Store/CountriesStore";
 
 
 function App() {
+  const { DarkMode } = useCountryStore();
   return (
-    <main className="main__container">
+    <main className="main__container" data-theme={DarkMode ? "dark" : "light"}>
       <Nabvar />
       <Routes>
       <Route path="/cardcountry/:countryName" element={<CardCountry/>}/>
